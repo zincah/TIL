4aa9b60734414c602f9c5d64d394c9b00736b446
moonjiyoon <ske0830@gmail.com>
2025-06-08 21:36:05 +0900

    item 29 commit

---
+++ src/main/java/org/example/effective/chapter5/item29/Stack.java
@@ -0,0 +1,50 @@
+package org.example.effective.chapter5.item29;
+
+import java.util.Arrays;
+import java.util.EmptyStackException;
+
+public class Stack<E> {
+    private E[] elements;
+    private int size = 0;
+    private static final int DEFAULT_INITIAL_CAPACITY = 16;
+
+    @SuppressWarnings("unchecked")
+    public Stack() {
+        elements = (E[]) new Object[DEFAULT_INITIAL_CAPACITY];
+    }
+
+    public void push (E e) {
+        ensureCapacity();
+        elements[size++] = e;
+    }
+
+    public E pop() {
+        if (size == 0)
+            throw new EmptyStackException();
+        @SuppressWarnings("unchecked") E result = (E) elements[--size];
+        elements[size] = null;
+        return result;
+    }
+
+    public boolean isEmpty() {
+        return size == 0;
+    }
+
+    private void ensureCapacity() {
+        if (elements.length == size) {
+            elements = Arrays.copyOf(elements, 2 * size + 1);
+        }
+    }
+}
+
+class StackTest {
+    public static void main(String[] args) {
+        Stack<String> stack = new Stack<>();
+        for (String arg : args) {
+            stack.push(arg);
+        }
+        while (!stack.isEmpty()) {
+            System.out.println(stack.pop().toUpperCase());
+        }
+    }
+}
